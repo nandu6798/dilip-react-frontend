f769c0a691e9b0b9bef3795d2a4ad6770b4bf8cb
nandu6798 <nandhini6798@gmail.com>
2025-05-08 01:17:05 +0530

    Code push

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const response = await axios.get('http://localhost:3001/api/tasks');
+    setTasks(response.data);
+  };
+
+  const addTask = async () => {
+    await axios.post('http://localhost:3001/api/tasks', { title });
+    setTitle('');
+    fetchTasks();
+  };
+
+  return (
+    <div className="App">
+      <h1>Task Manager</h1>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Enter task title"
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
